905b86e79314b49d52316da28a1b2ae3f2060039
Aqz <Kzak.Gluhov@gmail.com>
2022-04-04 13:34:14 +0300

    Revert "java2"
    This reverts commit 53d22a3354402990ee4fbb7cd658d6a654279583.

--- BodyMassIndex.java
+++
@@ -1,8 +0,0 @@
-public class BodyMassIndex {
-    public double calculate(double mass, double height) {
-
-        double bodyMassIndex = mass / (height*height);
-
-        return bodyMassIndex;
-    }
-}
